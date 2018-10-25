3e8a7522ce6082e9df4426732654ed983c499d42
Jonathan Torres <jonathantorres41@gmail.com>
2018-10-24 22:53:06 -0400

    Add initial test runner

---
+++ tests/unittest.h
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+typedef char *(*test_func)();
+
+int num_of_tests = 0;
+int num_of_assertions = 0;
+
+void static inline run_test(test_func func)
+{
+    num_of_tests++;
+
+    // execute the function, if it returns a string,
+    // this means that the test failed.
+    // Exit the program with the failure message
+    char *test_result = func();
+
+    if (test_result != NULL) {
+        fprintf(stderr, "[TEST FAIL]: %s\n", test_result);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void static inline assert(int expr, char *msg)
+{
+    num_of_assertions++;
+
+    if (!expr) {
+        fprintf(stderr, "[ASSERTION FAIL]: %s\n", msg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void static inline start_tests(char *test_name)
+{
+    printf("Running %s...", test_name);
+}
+
+void static inline end_tests()
+{
+    printf("Passed. %d tests and %d assertions.\n", num_of_tests, num_of_assertions);
+}
