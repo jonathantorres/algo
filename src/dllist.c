89348e0c8539feae4861d7603e2c0a0cc9251fc4
Jonathan Torres <jonathantorres41@gmail.com>
2018-11-07 23:16:35 -0500

    Finish functions for the doubly linked list

--- src/dllist.c
+++ src/dllist.c
@@ -34,6 +34,7 @@ void destroy_node(dllist_node *node)
     node = NULL;
 }
 
+// create a new list
 dllist *dllist_new()
 {
     dllist *new_list = malloc(sizeof(dllist));
@@ -49,19 +50,17 @@ dllist *dllist_new()
     return new_list;
 }
 
-// TODO: use list->prev here, to simply this code
+// remove all the values in the list
 void dllist_clear(dllist *list)
 {
     if (list->first != NULL) {
         dllist_node *current_node = list->first;
-        dllist_node *prev_node = NULL;
 
         while (current_node->next != NULL) {
-            prev_node = current_node;
             current_node = current_node->next;
 
-            if (prev_node) {
-                destroy_node(prev_node);
+            if (current_node->prev) {
+                destroy_node(current_node->prev);
                 list->length--;
             }
         }
@@ -75,6 +74,7 @@ void dllist_clear(dllist *list)
     }
 }
 
+// destroy the list
 void dllist_destroy(dllist *list)
 {
     if (!list) {
@@ -87,4 +87,208 @@ void dllist_destroy(dllist *list)
     free(list);
 
     list = NULL;
+}
+
+// insert at the end
+void dllist_push(dllist *list, void *value)
+{
+    if (!list) {
+        fputs("Must provide a dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    dllist_node *new_node = create_node(value);
+    list->length++;
+
+    // list is empty, this is the first element
+    if (list->first == NULL) {
+        list->first = new_node;
+        return;
+    }
+
+    dllist_node *current_node = list->first;
+
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+    }
+
+    new_node->prev = current_node;
+    current_node->next = new_node;
+}
+
+// insert at the beginning
+void dllist_shift(dllist *list, void *value)
+{
+    if (!list) {
+        fputs("Must provide a dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    dllist_node *new_node = create_node(value);
+    list->length++;
+
+    // list is empty, this is the first element
+    if (list->first == NULL) {
+        list->first = new_node;
+        return;
+    }
+
+    new_node->next = list->first;
+    list->first = new_node;
+}
+
+// remove the first node and return it
+void *dllist_unshift(dllist *list)
+{
+    if (!list) {
+        fputs("Must provide a dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // list is empty, return nothing
+    if (list->first == NULL) {
+        return NULL;
+    }
+
+    list->length--;
+
+    // list has just 1 node
+    if (list->first->next == NULL) {
+        void *value = list->first->value;
+        destroy_node(list->first);
+
+        return value;
+    }
+
+    dllist_node *new_first = list->first->next;
+    void *value = list->first->value;
+
+    destroy_node(list->first);
+    new_first->prev = NULL;
+    list->first = new_first;
+
+    return value;
+}
+
+// remove the last node and return it
+void *dllist_pop(dllist *list)
+{
+    if (!list) {
+        fputs("Must provide a dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // list is empty, return nothing
+    if (list->first == NULL) {
+        return NULL;
+    }
+
+    list->length--;
+
+    // list has just 1 node
+    if (list->first->next == NULL) {
+        void *value = list->first->value;
+        destroy_node(list->first);
+        list->first = NULL;
+
+        return value;
+    }
+
+    dllist_node *current_node = list->first;
+
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+    }
+
+    void *value = current_node->value;
+    current_node->prev->next = NULL;
+    destroy_node(current_node);
+    current_node = NULL;
+
+    return value;
+}
+
+// remove node whose value is {value}
+void dllist_remove(dllist *list, void *value, dllist_cmp cmp)
+{
+    if (!list) {
+        fputs("Must provide a valid dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // list is empty, return nothing
+    if (list->first == NULL) {
+        return;
+    }
+
+    // list has just 1 node
+    if (list->first->next == NULL) {
+        void *node_value = list->first->value;
+
+        if (cmp(node_value, value) == 0) {
+            dllist_pop(list);
+        }
+
+        return;
+    }
+
+    dllist_node *current_node = list->first;
+
+    // check the first one
+    if (cmp(current_node->value, value) == 0) {
+        dllist_node *next_node = current_node->next;
+        next_node->prev = NULL;
+        list->first = next_node;
+        destroy_node(current_node);
+        list->length--;
+        return;
+    }
+
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+
+        if (cmp(current_node->value, value) == 0) {
+            // remove the node
+            current_node->prev->next = current_node->next;
+            current_node->next->prev = current_node->prev;
+            destroy_node(current_node);
+            list->length--;
+            break;
+        }
+    }
+}
+
+// check to see if value {value} exists in the list
+int dllist_exists(dllist *list, void *value, dllist_cmp cmp)
+{
+    if (!list) {
+        fputs("Must provide a valid dllist.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // list is empty, not found
+    if (list->first == NULL) {
+        return 0;
+    }
+
+    dllist_node *current_node = list->first;
+
+    // check the first one
+    if (cmp(current_node->value, value) == 0) {
+        return 1;
+    }
+
+    int found = 0;
+
+    // check the rest
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+
+        if (cmp(current_node->value, value) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    return found;
 }
--- tests/dllist_test.c
+++ tests/dllist_test.c
@@ -27,6 +27,11 @@ void dllist_print(dllist *list)
     printf("}\n");
 }
 
+int cmp_func(void *a, void *b)
+{
+    return strcmp((char*) a, (char*) b);
+}
+
 char *test_create()
 {
     dllist *list = dllist_new();
@@ -40,37 +45,172 @@ char *test_create()
     return NULL;
 }
 
-// char *test_destroy()
-// {
-//     dllist *list = dllist_new();
-//     dllist_push(list, "one");
-//     dllist_push(list, "two");
-//     dllist_push(list, "three");
-//     dllist_destroy(list);
-//     return NULL;
-// }
+char *test_push()
+{
+    dllist *list = dllist_new();
 
-// char *test_clear()
-// {
-//     dllist *list = dllist_new();
-//     dllist_push(list, "one");
-//     dllist_push(list, "two");
-//     dllist_push(list, "three");
-//     dllist_clear(list);
+    char *john = "John";
+    char *luis = "Luis";
+    char *chris = "Chris";
+    char *mar = "Marjorie";
 
-//     assert(list->length == 0, "List length must be 0");
+    dllist_push(list, john);
+    dllist_push(list, luis);
+    dllist_push(list, chris);
+    dllist_push(list, mar);
 
-//     dllist_destroy(list);
+    assert(list->first->value == john, "John must be the first node in the list");
+    assert(list->length == 4, "Length of the list must be 4");
 
-//     return NULL;
-// }
+    dllist_destroy(list);
+    return NULL;
+}
+
+char *test_destroy()
+{
+    dllist *list = dllist_new();
+
+    dllist_push(list, "one");
+    dllist_push(list, "two");
+    dllist_push(list, "three");
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_clear()
+{
+    dllist *list = dllist_new();
+
+    dllist_push(list, "one");
+    dllist_push(list, "two");
+    dllist_push(list, "three");
+    dllist_clear(list);
+
+    assert(list->length == 0, "List length must be 0");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_shift()
+{
+    dllist *list = dllist_new();
+
+    dllist_push(list, "one");
+    dllist_push(list, "two");
+    dllist_push(list, "three");
+
+    assert(list->length == 3, "List length must be 3");
+    dllist_shift(list, "zero");
+    assert(list->length == 4, "List length must be 4");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_unshift()
+{
+    dllist *list = dllist_new();
+    char *zero = "zero";
+    char *one = "one";
+    char *two = "two";
+    char *three = "three";
+
+    dllist_push(list, zero);
+    dllist_push(list, one);
+    dllist_push(list, two);
+    dllist_push(list, three);
+
+    assert(list->length == 4, "List length must be 4");
+    char *value = (char*) dllist_unshift(list);
+    assert(list->length == 3, "List length must be 3");
+    assert(strcmp(zero, value) == 0, "List should be equal");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_pop()
+{
+    dllist *list = dllist_new();
+    char *zero = "zero";
+    char *one = "one";
+    char *two = "two";
+    char *three = "three";
+
+    dllist_push(list, zero);
+    dllist_push(list, one);
+    dllist_push(list, two);
+    dllist_push(list, three);
+
+    assert(list->length == 4, "List length must be 4");
+    char *value = (char*) dllist_pop(list);
+    assert(list->length == 3, "List length must be 3");
+    assert(strcmp(three, value) == 0, "List should be equal");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_remove()
+{
+    dllist *list = dllist_new();
+    dllist_push(list, "zero");
+    dllist_push(list, "one");
+    dllist_push(list, "two");
+    dllist_push(list, "three");
+
+    assert(list->length == 4, "List length must be 4");
+    dllist_remove(list, "zero", cmp_func);
+    assert(list->length == 3, "List length must be 3");
+    dllist_remove(list, "two", cmp_func);
+    assert(list->length == 2, "List length must be 2");
+    dllist_remove(list, "one", cmp_func);
+    assert(list->length == 1, "List length must be 1");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
+
+char *test_exists()
+{
+    dllist *list = dllist_new();
+    dllist_push(list, "zero");
+    dllist_push(list, "one");
+    dllist_push(list, "two");
+    dllist_push(list, "three");
+
+    assert(list->length == 4, "List length must be 4");
+    assert(dllist_exists(list, "zero", cmp_func) == 1, "zero should exist in the list");
+    assert(dllist_exists(list, "one", cmp_func) == 1, "one should exist in the list");
+    assert(dllist_exists(list, "two", cmp_func) == 1, "two should exist in the list");
+    assert(dllist_exists(list, "three", cmp_func) == 1, "three should exist in the list");
+    assert(dllist_exists(list, "foo", cmp_func) == 0, "foo shouldn't exist in the list");
+    assert(list->length == 4, "List length must be 4");
+
+    dllist_destroy(list);
+
+    return NULL;
+}
 
 int main()
 {
     start_tests("doubly linked list tests");
     run_test(test_create);
-    // run_test(test_destroy);
-    // run_test(test_clear);
+    run_test(test_push);
+    run_test(test_destroy);
+    run_test(test_clear);
+    run_test(test_shift);
+    run_test(test_unshift);
+    run_test(test_pop);
+    run_test(test_remove);
+    run_test(test_exists);
     end_tests();
 
     return 0;
