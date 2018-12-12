ac95a702a4a79f52c59772f0c7d1d2062a87a98b
Jonathan Torres <jonathantorres41@gmail.com>
2018-12-11 21:51:26 -0500

    Initial work on cbuffer

--- src/cbuffer.c
+++ src/cbuffer.c
@@ -1,7 +1,58 @@
 #include "cbuffer.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-void *cbuffer_create()
+void *cbuffer_create(unsigned int length)
 {
-    return NULL;
+    cbuffer *buffer = malloc(sizeof(cbuffer));
+
+    if (!buffer) {
+        fputs("Not enough memory.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    buffer->length = length;
+    buffer->start = 0;
+    buffer->end = 0;
+    buffer->buffer = malloc(buffer->length);
+
+    if (!buffer->buffer) {
+        fputs("Not enough memory.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return buffer;
+}
+
+void cbuffer_clear(cbuffer *buffer)
+{
+    if (!buffer) {
+        fputs("Must provide a valid cbuffer.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (buffer->buffer) {
+        free(buffer->buffer);
+        buffer->buffer = malloc(buffer->length);
+
+        if (!buffer->buffer) {
+            fputs("Not enough memory.", stderr);
+            exit(EXIT_FAILURE);
+        }
+        buffer->start = 0;
+        buffer->end = 0;
+    }
+}
+
+void cbuffer_destroy(cbuffer *buffer)
+{
+    if (!buffer) {
+        fputs("Must provide a valid cbuffer.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (buffer->buffer) {
+        free(buffer->buffer);
+    }
+    free(buffer);
 }
--- tests/cbuffer_test.c
+++ tests/cbuffer_test.c
@@ -1,15 +1,38 @@
 #include "unittest.h"
 #include "cbuffer.h"
 
-char *test_init()
+char *test_create()
 {
+    cbuffer *buf = cbuffer_create(10);
+    assert(buf != NULL, "Buffer should not be NULL");
+    assert(buf->start == 0, "Buffer's start should be at 0");
+    assert(buf->end == 0, "Buffer's end should be at 0");
+    assert(buf->length == 10, "Buffer length should be 10");
+    assert(buf->buffer != NULL, "Buffer's buffer should not be NULL");
+    cbuffer_destroy(buf);
+
+    return NULL;
+}
+
+char *test_clear()
+{
+    return NULL;
+}
+
+char *test_destroy()
+{
+    cbuffer *buf = cbuffer_create(10);
+    cbuffer_destroy(buf);
+
     return NULL;
 }
 
 int main()
 {
     start_tests("cbuffer tests");
-    run_test(test_init);
+    run_test(test_create);
+    run_test(test_clear);
+    run_test(test_destroy);
     end_tests();
 
     return 0;
