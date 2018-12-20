03e29d101cc9eee089087ff8c799878920cde155
Jonathan Torres <jonathantorres41@gmail.com>
2018-12-19 22:27:52 -0500

    More work on the hash table

--- src/htable.c
+++ src/htable.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include "htable.h"
 
+size_t fnv1a_hash(const char* cp)
+{
+    size_t hash = 0x811c9dc5;
+    while (*cp) {
+        hash ^= (unsigned char) *cp++;
+        hash *= 0x01000193;
+    }
+
+    return hash;
+}
+
 htable *htable_create(htable_compare cmp)
 {
     htable *_htable = malloc(sizeof(htable));
@@ -16,3 +27,48 @@ htable *htable_create(htable_compare cmp)
 
     return _htable;
 }
+
+void htable_destroy(htable *_htable)
+{
+    if (!_htable) {
+        fputs("Must provide a hashtable.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (_htable->buckets) {
+        for (unsigned int i = 0; i < _htable->buckets->length; i++) {
+            void *bucket = array_get(_htable->buckets, i);
+            if (bucket) {
+                array_destroy(bucket);
+            }
+        }
+        array_destroy(_htable->buckets);
+    }
+    free(_htable);
+}
+
+unsigned int htable_set(htable *_htable, void *key, void *value)
+{
+    if (!_htable) {
+        fputs("Must provide a hashtable.", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int bucket_index = fnv1a_hash(key) % NUM_OF_BUCKETS;
+    if (bucket_index >= NUM_OF_BUCKETS || bucket_index <= 0) {
+        return 1;
+    }
+
+    void *bucket = array_get(_htable->buckets, bucket_index);
+    if (!bucket) {
+        bucket = array_create(NUM_OF_BUCKETS, sizeof(htable_node*));
+        array_set(_htable->buckets, bucket, bucket_index);
+    }
+    // create the node
+    // push the node into the bucket
+}
+
+
+
+
+
--- tests/htable_test.c
+++ tests/htable_test.c
@@ -1,8 +1,28 @@
+#include <string.h>
 #include "unittest.h"
 #include "htable.h"
 
+int htable_compare_fn(void *a, void *b)
+{
+    return strcmp((char*)a, (char*)b);
+}
+
 char *test_create()
 {
+    htable *_htable = htable_create(htable_compare_fn);
+    assert(_htable != NULL, "Hashtable cannot be NULL");
+    assert(_htable->cmp == htable_compare_fn, "Hashtable must use the correct compare function");
+    assert(_htable->buckets != NULL, "Hashtable buckets cannot be NULL");
+    htable_destroy(_htable);
+
+    return NULL;
+}
+
+char *test_destroy()
+{
+    htable *_htable = htable_create(htable_compare_fn);
+    htable_destroy(_htable);
+
     return NULL;
 }
 
@@ -10,6 +30,7 @@ int main()
 {
     start_tests("hash table tests");
     run_test(test_create);
+    run_test(test_destroy);
     end_tests();
 
     return 0;
