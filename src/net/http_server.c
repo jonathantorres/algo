ab59afb5dbf2494a1d82d447c742d814ba2f9370
Jonathan Torres <jonathantorres41@gmail.com>
2019-03-10 14:12:40 -0400

    Minor improvements on the http server

--- src/net/http_server.c
+++ src/net/http_server.c
@@ -7,48 +7,57 @@
 #include<unistd.h>
 
 int main() {
-    int create_socket, new_socket;
+    int server_fd, client_fd;
     socklen_t addrlen;
     int bufsize = 1024;
+    int port = 3490;
     char *buffer = malloc(bufsize);
     struct sockaddr_in address;
 
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
-        printf("The socket was created\n");
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("server: socket()");
+        exit(1);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(3490);
+    address.sin_port = htons(port);
 
-    if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0){
-        printf("Binding Socket\n");
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("server: bind()");
+        close(server_fd);
+        exit(1);
     }
 
+    if (listen(server_fd, 10) < 0) {
+        perror("server: listen()");
+        close(server_fd);
+        exit(1);
+    }
+
+    printf("Server running on port: %d\n", port);
+
     while (1) {
-        if (listen(create_socket, 10) < 0) {
-            perror("server: listen");
-            exit(1);
-        }
+        client_fd = accept(server_fd, (struct sockaddr *) &address, &addrlen);
 
-        if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) < 0) {
+        if (client_fd < 0) {
             perror("server: accept");
-            exit(1);
+            continue;
         }
 
-        if (new_socket > 0) {
+        if (client_fd > 0) {
             printf("The Client is connected...\n");
         }
 
-        recv(new_socket, buffer, bufsize, 0);
+        recv(client_fd, buffer, bufsize, 0);
         printf("%s\n", buffer);
-        // write(new_socket, "hello world\n", 12);
-        write(new_socket, "HTTP/1.1 200 OK\n", 16);
-        write(new_socket, "Content-length: 46\n", 19);
-        write(new_socket, "Content-Type: text/html\n\n", 25);
-        write(new_socket, "<html><body><h1>Hello world</h1></body></html>", 46);
-        close(new_socket);
+        // write(client_fd, "hello world\n", 12);
+        write(client_fd, "HTTP/1.1 200 OK\n", 16);
+        write(client_fd, "Content-length: 46\n", 19);
+        write(client_fd, "Content-Type: text/html\n\n", 25);
+        write(client_fd, "<html><body><h1>Hello world</h1></body></html>", 46);
+        close(client_fd);
     }
-    close(create_socket);
+    close(server_fd);
     return 0;
 }
