91a4d7b11e26b9e6891b90ef369798b0e7838a72
Jonathan Torres <jonathantorres41@gmail.com>
2019-12-29 17:03:01 -0500

    Some work on dfs

--- tests/graph_test.c
+++ tests/graph_test.c
@@ -24,6 +24,12 @@ bool processed[MAXV+1];
 bool discovered[MAXV+1];
 int parent[MAXV+1];
 
+// for dfs
+int time;
+int entry_time[MAXV+1];
+int exit_time[MAXV+1];
+bool finished;
+
 graph *graph_create(int nvertices, bool directed)
 {
     graph *new_graph = malloc(sizeof(graph));
@@ -96,11 +102,25 @@ void graph_init_search(graph *graph)
     if (!graph) {
         return;
     }
+    time = 0;
+    finished = false;
 
     for (int i = 1; i <= graph->nvertices; i++) {
         processed[i] = false;
         discovered[i] = false;
         parent[i] = -1;
+        entry_time[i] = 0;
+        exit_time[i] = 0;
+    }
+}
+
+void dfs_find_path(int start, int end, int parents[])
+{
+    if ((start == end) || (end == -1)) {
+        printf("\n%d", start);
+    } else {
+        dfs_find_path(start, parents[end], parents);
+        printf(" %d", end);
     }
 }
 
@@ -114,9 +134,20 @@ void graph_process_vertex_late(int vertex)
     printf("processed vertex late: %d\n", vertex);
 }
 
-void graph_process_edge(int a, int b)
+void graph_bfs_process_edge(int a, int b)
 {
     printf("processed edge: (%d, %d)\n", a, b);
+}
+
+void graph_dfs_process_edge(int a, int b)
+{
+    printf("processed edge: (%d, %d)\n", a, b);
+    if (parent[a] != b) {
+        printf("Cycle from %d to %d:", a, b);
+        dfs_find_path(b, a, parent);
+        printf("\n\n");
+        finished = true;
+    }
 }
 
 void graph_bfs(graph *graph, int start)
@@ -145,7 +176,7 @@ void graph_bfs(graph *graph, int start)
         while (_edge_node != NULL) {
             suc_vertex = _edge_node->y;
             if ((processed[suc_vertex] == false) || graph->directed) {
-                graph_process_edge(cur_vertex, suc_vertex);
+                graph_bfs_process_edge(cur_vertex, suc_vertex);
             }
             if (discovered[suc_vertex] == false) {
                 queue_enqueue(_queue, &suc_vertex);
@@ -157,6 +188,44 @@ void graph_bfs(graph *graph, int start)
         graph_process_vertex_late(cur_vertex);
     }
     queue_destroy(_queue);
+}
+
+void graph_dfs(graph *graph, int v)
+{
+    if (!graph) {
+        return;
+    }
+    edgenode *_edge_node;
+    int y;
+
+    if (finished) {
+        return;
+    }
+    discovered[v] = true;
+    time = time+1;
+    entry_time[v] = time;
+
+    graph_process_vertex_early(v);
+
+    _edge_node = graph->edges[v];
+    while (_edge_node != NULL) {
+        y = _edge_node->y;
+        if (discovered[y] == false) {
+            parent[y] = v;
+            graph_dfs_process_edge(v, y);
+            graph_dfs(graph, y);
+        } else if ((!processed[y]) || (graph->directed)) {
+            graph_dfs_process_edge(v, y);
+            if (finished) {
+                return;
+            }
+            _edge_node = _edge_node->next;
+        }
+        graph_process_vertex_late(v);
+        time = time+1;
+        exit_time[v] = time;
+        processed[v] = true;
+    }
 }
 
 char *graph_create_test()
@@ -193,11 +262,31 @@ char *graph_bfs_test()
     return NULL;
 }
 
+char *graph_dfs_test()
+{
+    graph *d_graph = graph_create(6, false);
+    graph_insert_edge(d_graph, 1, 2, false);
+    graph_insert_edge(d_graph, 1, 4, false);
+    graph_insert_edge(d_graph, 2, 3, false);
+    graph_insert_edge(d_graph, 2, 6, false);
+    graph_insert_edge(d_graph, 3, 4, false);
+    graph_insert_edge(d_graph, 3, 6, false);
+    graph_insert_edge(d_graph, 4, 5, false);
+    graph_insert_edge(d_graph, 5, 6, false);
+    graph_print_stdout(d_graph);
+
+    graph_init_search(d_graph);
+    graph_dfs(d_graph, 1);
+
+    return NULL;
+}
+
 int main()
 {
     start_tests("graph tests");
     run_test(graph_create_test);
     run_test(graph_bfs_test);
+    run_test(graph_dfs_test);
     end_tests();
 
     return 0;
