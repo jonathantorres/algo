f308e58e76499c76c24105c66c69f1439582e82c
Jonathan Torres <jonathantorres41@gmail.com>
2022-03-01 23:37:39 -0500

    Tweaks on inet_pton_loose

--- inet_pton_loose.c
+++ inet_pton_loose.c
@@ -19,11 +19,13 @@ int inet_pton_loose(int family, const char *strptr, void *addrptr)
 {
     int rc = inet_pton(family, strptr, addrptr);
     if (rc == 0) {
-        int rc2 = inet_aton(strptr, addrptr);
-        if (rc2 == 0) {
+        rc = inet_aton(strptr, addrptr);
+
+        if (rc == 0) {
             return 1;
         }
-        return rc2;
+        return rc;
     }
+
     return rc;
 }
