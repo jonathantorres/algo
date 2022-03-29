5663dbd6c0c7254b062942c98b885793abb44baa
Jonathan Torres <jonathantorres41@gmail.com>
2022-03-28 21:40:55 -0400

    Tweaks on programs to get host names

--- get_host_names.c
+++ get_host_names.c
@@ -1,9 +1,9 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <errno.h>
 
 int main(int argc, char **argv)
 {
